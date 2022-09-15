58d1997fc74351e464f061d367e0ddefa04472a3
nicolas nguon <nicolas.nguon@checkmyguest.fr>
2022-09-15 20:34:30 +0200

    controller declaration

---
+++ app/controllers/declarations_controller.rb
@@ -0,0 +1,47 @@
+class DeclarationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @declarations = Declaration.all
+  end
+
+  def show
+  end
+
+  def new
+    @declaration = Declaration.new
+  end
+
+  def create
+    @declaration = Declaration.new(declaration_params)
+    user = current_user
+    @declaration.user = user
+    if @declaration.save!
+      redirect_to *(@declaration)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @declaration = Declaration.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_declaration
+    @declaration = Declaration.find(params[:id])
+  end
+
+  def declaration_params
+    params.require(:declaration).permit(:user_id, :solo)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :declarations do
-    resources :declaration_items, only: %i(new create edit update destroy)
+    resources :declaration_items, only: %i(new create edit update)
   end
   resources :declaration_items, only: %i(destroy)
 end
